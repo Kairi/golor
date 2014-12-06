d2b9d93d65c6b316314f982e4910b9433044874e
Kairi OKUMURA <kairi199088@gmail.com>
2014-12-06 11:54:10 +0900

    fix color code type

--- golor.go
+++ golor.go
@@ -1,22 +1,23 @@
-package golar
+package golor
 
 import (
 	"fmt"
 )
 
-var (
-	black_code = "30"
-	red_code = "31"
-	green_code = "32"
-	yellow_code = "33"
-	blue_code = "34"
-	magenta_code = "35"
-	syan_code = "36"
-	white_code = "37"
+type ColorCode string
+const (
+	BLACK = "30"
+	RED = "31"
+	GREEN = "32"
+	YELLOW = "33"
+	BLUE = "34"
+	MAGENTA = "35"
+	SYAN = "36"
+	WHITE = "37"
 )
 
-func colorlize(str, color string) {
-	color_prefix := "\x1b[" + color + "m"
+func Colorlize(str string , color ColorCode) {
+	color_prefix := "\x1b[" + string(color) + "m"
 	color_suffix := "\x1b[0m"
 	fmt.Println(color_prefix + str + color_suffix)
 }
