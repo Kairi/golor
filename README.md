3e38836e0332005c6b0f8d83c2e91e77bf3bfaf4
Kairi OKUMURA <kairi199088@gmail.com>
2014-12-06 21:47:19 +0900

    add sample statement in README and ColorPrintln function

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ golor
 =====
 
 golang lib for colorlize ouput
+
+**Instllation
+
+```bash
+go get github.com/Kairi/golor
+```
+
+**Sample
+```go
+import (
+    "fmt"
+    "github.com/Kairi/golor"
+)
+
+func main() {
+    fmt.Println("basic string")
+    fmt.Println(golor.ColorString("yellow string", golor.YELLOW))
+    golor.ColorPrintln("blue string", golor.YELLOW)    
+}
+```
+    
+
+
--- golor.go
+++ golor.go
@@ -5,6 +5,7 @@ import (
 )
 
 type ColorCode string
+
 const (
 	BLACK = "30"
 	RED = "31"
@@ -16,9 +17,14 @@ const (
 	WHITE = "37"
 )
 
-func Colorlize(str string , color ColorCode) {
+func ColorPrintln(str string , color ColorCode) {
 	color_prefix := "\x1b[" + string(color) + "m"
 	color_suffix := "\x1b[0m"
 	fmt.Println(color_prefix + str + color_suffix)
 }
 
+func ColorString(str string, color ColorCode) string {
+	color_prefix := "\x1b[" + string(color) + "m"
+	color_suffix := "\x1b[0m"
+	return color_prefix + str + color_suffix
+}
